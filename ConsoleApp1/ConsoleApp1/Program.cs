ba31c7bdf4f9ec7276295bb7c9865e093b3241c7
Nargiz Ganiyeva <tu7lyw21z@aztu.local>
2022-03-11 09:08:45 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //public string name="Robin Hood";
+            Book book = new Book();
+            book .name = "Robin Hood";
+            book .costPrice = 5;
+            book .saledPrice = 9;
+            book .authorName = "J.Walker";
+            book .pageCount = 217;
+            book .discountPercent = 5;
+            book.GetInto();
+
+        }
+        internal class Book
+        {
+            //1.fields
+            public string name;
+            public int costPrice ; //default deyeri bu hissede veririk (15ci setir )
+            public int saledPrice;
+            public string authorName;
+            public int pageCount;
+            public int discountPercent;
+
+
+            public void GetInto()
+            {
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"CostPrice: {costPrice}");
+                Console.WriteLine($"SaledPrice: {saledPrice}");
+                Console.WriteLine($"AuthorName: { authorName}" );
+                Console.WriteLine($"PageCount:{ pageCount}" );
+                Console.WriteLine($"DiscountPercent:{discountPercent }" );
+
+            }
+
+
+        }
+    }
+}
